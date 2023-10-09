f42d003c98ecef2e97296b0af5e03013f9e96098
João Paulo <joaopaulo.eu29@hotmail.com>
2023-10-09 10:41:37 -0300

    Conectando com servidor

--- src/main/java/servidor/Servidor.java
+++ src/main/java/servidor/Servidor.java
@@ -2,73 +2,65 @@ package servidor;
 
 import Interface.Interface;
 import conexao.Paciente;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Servidor implements Serializable {
+public class Servidor {
 
     private static final int PORTA = 2000;
-    private static ArrayList<Paciente> paciente = new ArrayList<>();
-    private static ArrayList<String> diagnosticoApriori;
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-
-        ServerSocket serverSocket;
-
+    public static void main(String[] args) {
         try {
-            serverSocket = new ServerSocket(PORTA);
-            System.out.println("Servidor esperando conexões na porta: " + PORTA);
-        } catch (BindException e) {
-            e.printStackTrace();
-            return;
+            iniciarServidor();
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        new Interface().setVisible(true);
-        
-        while (true) {
-            Socket socket = serverSocket.accept();
+    }
+
+    private static void iniciarServidor() throws IOException, ClassNotFoundException {
+        try (ServerSocket serverSocket = new ServerSocket(PORTA)) {
+            System.out.println("Servidor esperando conexões na porta: " + PORTA);
+            new Interface().setVisible(true);
+
+            while (true) {
+                esperarConexaoEProcessar(serverSocket);
+            }
+        } catch (BindException e) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    private static void esperarConexaoEProcessar(ServerSocket serverSocket) throws IOException, ClassNotFoundException {
+        try (Socket socket = serverSocket.accept();
+             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())) {
+
             System.out.println("Cliente conectado: " + socket.getInetAddress());
 
-            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
             Paciente sintomas = (Paciente) inputStream.readObject();
-            
-            System.out.println("Recebi do cliente os sintomas:" + sintomas );
+            System.out.println("Recebi do cliente os sintomas:" + sintomas);
 
-            paciente.add(sintomas);
-
-            diagnosticoApriori = realizarApriori(paciente);
-
-            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            ArrayList<String> diagnosticoApriori = realizarApriori(sintomas);
 
             outputStream.writeObject(diagnosticoApriori);
-
-            socket.close();
         }
-
     }
 
-    public static int getPORTA() {
-        return PORTA;
-    }
+    private static ArrayList<String> realizarApriori(Paciente consulta) {
+        ArrayList<String> diagnostico = new ArrayList<>();
 
-    public static ArrayList<String> getDiagnosticoAutomatico() {
-        return diagnosticoApriori;
-    }
+        // Lógica para gerar diagnóstico a partir da consulta do paciente
+        // diagnostico.add("Diagnosticado como dengue pelo médico Asdrubal");
 
-    private static ArrayList<String> realizarApriori(ArrayList<Paciente> consultas) {
-        ArrayList<String> diagnostico = new ArrayList();
-        ArrayList<String> sintomas = new ArrayList();
-
-        //diagnostico.add("Diagnosticado como dengue pelo médico Asdrubal");
-
-        consultas.get(0).setDiagnosticos(diagnostico);
-        diagnosticoApriori = (ArrayList<String>) consultas.get(0).getDiagnosticos();
-        return diagnosticoApriori;
+        consulta.setDiagnosticos(diagnostico);
+        return (ArrayList<String>) consulta.getDiagnosticos();
     }
 }
